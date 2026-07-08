ab0cdfd0282d00c28d9c79e1e5224afbe2a48cbb
ezequiel <ezequieloliveiraes@outlook.com>
2026-07-08 13:20:03 -0300

    feat(removing) removing arquives

--- dist/models/contentCatalog.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContentCatalogModel = void 0;
-const mongoose_1 = require("mongoose");
-const contentCatalogSchema = new mongoose_1.Schema({
-    key: { type: String, required: true, unique: true },
-    items: { type: [mongoose_1.Schema.Types.Mixed], required: true, default: [] },
-}, { timestamps: true });
-exports.ContentCatalogModel = (0, mongoose_1.model)("ContentCatalog", contentCatalogSchema);
